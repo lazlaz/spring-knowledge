54a2f41ef37a65dca7c77e8cf3ed304ad94f844e
zhouxingting <zxtzxt140420>
2020-07-14 22:51:27 +0800

    spring mabatis基本结合示例

--- shiro-kownledge/src/main/java/com/laz/spring/knowledge/shiro/model/User.java
+++ shiro-kownledge/src/main/java/com/laz/spring/knowledge/shiro/model/User.java
@@ -11,8 +11,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class User {
 	 private static final long serialVersionUID = -5440372534300871944L;
-	    
-    private Integer id;
+
+	private Integer id;
     private String userName;
     private String password;
     private Date createTime;
