ddd4e19d400734008cb609c64e8dcd04276ae783
liuzhai <578091472@qq.com>
2020-10-28 15:18:56 +0800

    顺序消息

---
+++ spring-mq-knowledge/spring-rocket-mq/src/main/java/com/laz/spring/rocket/mq/message/Demo06Message.java
@@ -0,0 +1,18 @@
+package com.laz.spring.rocket.mq.message;
+
+public class Demo06Message {
+	public static final String TOPIC = "DEMO_06";
+
+	/**
+	 * 编号
+	 */
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
