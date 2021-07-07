c43e89131d65fe7604a8273945f31e9b23adff4a
laz <578091472@qq.com>
2021-07-07 22:00:35 +0800

    webflux update

--- webflux-knoowledge/src/main/java/com/laz/knowledge/webflux/controller/HelloController.java
+++ webflux-knoowledge/src/main/java/com/laz/knowledge/webflux/controller/HelloController.java
@@ -2,14 +2,25 @@ package com.laz.knowledge.webflux.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.laz.knowledge.webflux.entiy.User;
+
 import reactor.core.publisher.Mono;
 
 
 @RestController
 public class HelloController {
 
-    @GetMapping("/hello")
-    public Mono<String> hello() {
-        return Mono.just("Welcome to reactive world ~");
-    }
+	 @GetMapping("/hello")
+	    public String hello() {
+	        return "Hello, WebFlux !";
+	    }
+
+	    @GetMapping("/user")
+	    public Mono<User> getUser() {
+	        User user = new User();
+	        user.setName("犬小哈");
+	        user.setDesc("欢迎关注我的公众号: 小哈学Java");
+	        return Mono.just(user);
+	    }
 }
---
+++ webflux-knoowledge/src/main/java/com/laz/knowledge/webflux/entiy/User.java
@@ -0,0 +1,28 @@
+package com.laz.knowledge.webflux.entiy;
+
+public class User {
+	 /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 描述
+     */
+    private String desc;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
